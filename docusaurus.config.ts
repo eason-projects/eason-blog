19e1bf437f55b485fd9bee21d411fdac78b8fe8f
Eason Guo <e10101@users.noreply.github.com>
2025-03-09 22:20:46 +0800

    Update baseUrl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   url: 'https://eason-projects.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/blog/',
+  baseUrl: '/eason-blog/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
