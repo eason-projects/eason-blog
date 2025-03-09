48f28d11147a9ff420dcbc88e52b5d4f2903ffd6
Eason Guo <e10101@users.noreply.github.com>
2025-03-09 22:13:28 +0800

    try to deploy

--- README.md
+++ README.md
@@ -16,6 +16,25 @@ $ pnpm start
 
 This command starts a local development server and opens up a browser window. Most changes are reflected live without having to restart the server.
 
+### Build
+
+```
+$ pnpm build
+```
+
+This command generates static content into the `build` directory and can be served using any static contents hosting service.
+
+### Deployment
+
+The site is automatically deployed to GitHub Pages when changes are pushed to the `main` branch, using GitHub Actions. The deployment workflow is defined in `.github/workflows/deploy.yml`.
+
+You can also manually deploy the site using:
+
+```
+$ GIT_USER=<Your GitHub username> pnpm deploy
+```
+
+This command is a convenient way to build the website and push to the `gh-pages` branch.
 
 ## Content
 
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -12,15 +12,15 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://eason-projects.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/blog/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'eason-projects', // Usually your GitHub org/user name.
+  projectName: 'blog', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
